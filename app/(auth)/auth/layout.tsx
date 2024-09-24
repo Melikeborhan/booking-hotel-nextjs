41b368f4f609b8b0469ebab27ff323d13f6324bc
Melike <22410051025@ogr.erbakan.edu.tr>
2024-09-24 11:15:07 +0300

    logın and regıster page desıgn

--- app/(auth)/auth/layout.tsx
+++ app/(auth)/auth/layout.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
 
 interface AuthLayoutProps {
     children: React.ReactNode
@@ -6,8 +8,34 @@ interface AuthLayoutProps {
 
 const AuthLayout = ({children}:AuthLayoutProps) => {
   return (
-    <div>
+    <div className='h-screen flex justify-center items-center bgone'>
+      <div className='hidden lg:block w-1/2 h-full'>
+        <Image
+          alt='login'
+          src="/login.jpg"
+          width={1080}
+          height={1920}
+          className='h-full w-full object-cover'
+        />
+
+      </div>
+      <div className='w-full lg:w-1/2 p-10 flex flex-col items-center'>
+        <div className='flex items-center mb-6'>
+          <div className='w-24 py-4 px-3 '>
+            <Link href="/">
+                <Image
+                  src="/hotel-logo.jpg"
+                  alt='logo'
+                  width={500}
+                  height={500}
+                  className='w-full'
+                />
+            </Link>
+          </div>
+        </div>
         {children}
+      </div>
+
     </div>
   )
 }
--- app/(auth)/auth/register/page.tsx
+++ app/(auth)/auth/register/page.tsx
@@ -1,8 +1,136 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+ 
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { useRouter } from 'next/navigation'
+import { Loader2 } from 'lucide-react'
+
+import { Label } from '@radix-ui/react-dropdown-menu'
+import Link from 'next/link'
+
+
+const formSchema = z.object({
+  username : z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  email: z.string().min(2, {
+    message: "Email must be at least 2 characters.",
+  }),
+  password: z.string().min(2, {
+      message: "Password must be at least 2 characters.",
+    }),
+  })
+
+ 
+
 
 const RegisterPage = () => {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "deneme1@gmail.com",
+      password: "deneme123"
+    },
+  })
+
+// 2. Define a submit handler.
+  const onSubmit=(data: z.infer<typeof formSchema>) => {
+    setIsLoading(true);
+  console.log(data)
+    setIsLoading(false);
+}
+
+
+
+
   return (
-    <div>RegisterPage</div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-4/5">
+      <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>username</FormLabel>
+              <FormControl>
+                <Input placeholder="username" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        
+
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="@gmail.com" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input placeholder="******" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className='bg-yellow-500 rounded-xl'>
+            {isLoading ?(
+              <>
+              <Loader2 size={20} className="animate-spin" /> Loading
+              </>
+            ):
+              <>Login</>
+            }
+
+
+        </Button>
+      </form>
+
+          <div className='mt-8'>
+            <Label className='flex flex-col items-center'>
+              Don't have an account?
+            </Label>
+            <Link href="/auth/login" className='mt-10 text-slate-500'>
+            Click here to create a new account
+            </Link>
+
+          </div>
+
+
+
+    </Form>
   )
 }
 
