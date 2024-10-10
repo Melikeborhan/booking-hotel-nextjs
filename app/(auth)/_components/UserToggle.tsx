247b073cb19041484cb420241e0e7f4fb81c4abc
Melike <22410051025@ogr.erbakan.edu.tr>
2024-10-10 14:08:17 +0300

    logout function on the icon

--- app/(auth)/_components/UserToggle.tsx
+++ app/(auth)/_components/UserToggle.tsx
@@ -1,17 +1,64 @@
-import { UserIcon } from 'lucide-react'
+"use client"
+import { User, UserIcon } from 'lucide-react'
 import Link from 'next/link'
-import React from 'react'
+
+import React, { useEffect, useState } from 'react'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { useRouter } from 'next/navigation'
+import { Toast } from '@radix-ui/react-toast'
 
 const UserToggle = () => {
+  const [user, setUser] = useState(null);
+  const router = useRouter();
+
+  useEffect(()=>{
+    const fetchUser = async()=>{
+      const authData = localStorage.getItem("pocketbase_auth");
+      if(authData){
+        const {token, model} = JSON.parse(authData);
+        setUser(model)
+      }
+    }
+    fetchUser();
+  },[])
   return (
-    <>
-   <Link href='/auth/login' >
-   <UserIcon className='text-white dark:text-white'/>
-   
-   </Link>
+    <div>
+      {user ? (
+        <div >
+          <DropdownMenu>
+          <DropdownMenuTrigger className='flex text-white text-xs justify-center items-center'>
+            <UserIcon className='text-white dark:text-white'/>  
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className='bg-white'>
+              <DropdownMenuLabel className='text-yellow-500'>{user.username}</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              
+              <DropdownMenuItem className='cursor-pointer' onClick={()=>{localStorage.removeItem("pocketbase_auth");setUser(null);router.push("/")}}>
+                Logout
+              </DropdownMenuItem>
+              <DropdownMenuItem>Subscription</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+
+      ):(
+        <Link href='/auth/login' >
+          <UserIcon className='text-white dark:text-white'/>
+      </Link>
+      
     
-    </>
-  )
+
+      )}
+   </div>
+    
+    )
 }
 
 export default UserToggle
